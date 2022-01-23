9cdb4c01615608af702280de47bfe1a0d90d8927
svetlana978 <cbeta279@gmail.com>
2022-01-23 16:32:46 +0300

    активное/не активное предложение

--- offerActivity.php
+++ offerActivity.php
@@ -1,8 +1,8 @@
 <?php 
-//session_start();
+session_start();
 //$_SESSION['auth'] = 0;
-print('2');
-echo("dfghjjjjjjj");
+
+
  $host = 'localhost';
  $user_enter = 'root';
  $password_enter = 'root';
@@ -13,21 +13,24 @@ $link = mysqli_connect($host, $user_enter, $password_enter, $dbName)
 
  mysqli_query($link, "SET NAMES 'utf8'");
 
-    $offer_id = $_POST ["element"];
+    $offer_id = $_POST ["data"];
     
-   // $offer_id = $_SESSION['i'];
-   var_dump("h + $offer_id");
-//  print('2');
+    // $of_id = $_SESSION['i'];
+    //  print($of_id);
+  //  print('2   ');
+   print($offer_id);
+  print('   2 ');
   
-     $query_current_activity = "SELECT activity FROM offers WHERE id = $offer_id";
+     $query_current_activity = "SELECT `activity` FROM `offers` WHERE `id` = '$offer_id'";
      $activity = mysqli_query($link, $query_current_activity) or die(mysqli_error($link));
      if (mysqli_fetch_assoc($activity) == 'YES') 
-         $query_change_activity = "UPDATE offers SET activity = 'NO' WHERE id = $offer_id";
+         $query_change_activity = "UPDATE `offers` SET `activity` = 'NO' WHERE `id` = '$offer_id'";
      else 
-        $query_change_activity = "UPDATE offers SET activity = 'YES' WHERE id = $offer_id";
+        $query_change_activity = "UPDATE `offers` SET `activity` = 'YES' WHERE `id` = '$offer_id'";
     
      $result = mysqli_query($link, $query_change_activity) or die(mysqli_error($link));
-  
-echo "cool";
+     //print($result);
+      
+echo "done";
  
 ?>
--- offerDisactive.js
+++ offerDisactive.js
@@ -2,12 +2,16 @@ document.addEventListener('change', function() {
     var check = event.target;
 
     if (check.tagName === 'INPUT' && check.type === 'checkbox') {
-        //console.log(check.id, check.value, check.checked);
-        element = Number(check.id);
+        console.log(check.id, check.checked);
+        //element = Number(check.id);
+        let data = {
+            str_num: Number(check.id),
+        };
         $.ajax({
             url: 'offerActivity.php',
             type: 'POST',
-            data: element,
+            // dataType: Object,
+            data: data,
             error: function() {
                 console.log('Ошибка!');
             },
